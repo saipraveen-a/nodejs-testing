c2087e0dfb3e4ceb57e650cb87b9c7736244c052
Sai Praveen Anumukonda <saipraveen-a@github.com>
2020-11-22 16:42:44 +0530

    Example tests with Mocha and Chai

--- test/example.test.js
+++ test/example.test.js
@@ -7,3 +7,13 @@ describe('Simple Math Test', () => {
         assert.strictEqual(3 * 3, 9);
     });
 });
+
+const expect = require('chai').expect;
+describe('Simple Math Test With Chai', () => {
+ it('should return 2', () => {
+        expect(1 + 1).to.equal(2);
+    });
+ it('should return 9', () => {
+        expect(3 * 3).to.equal(9);
+    });
+});
